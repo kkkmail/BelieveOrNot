7345d91376be842fd7fee0a6af3130257f59ecff
Konstantin Konstantinov <kkk@jfcglobal.net>
2025-08-10 19:39:02 -0400

    20250810

---
+++ src/Believe.App/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using Believe.Core;
+
+namespace Believe.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Believe App!");
+            var core = new Class1();
+            core.DoSomething();
+        }
+    }
+}
---
+++ src/Believe.Core/Class1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Believe.Core
+{
+    public class Class1
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Doing something in the core library.");
+        }
+    }
+}
