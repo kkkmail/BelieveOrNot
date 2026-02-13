9251f5c83b61ad8fa5623b8d06143e39157d94bb
kkkmail <kkk@jfcglobal.net>
2026-02-12 22:06:38 -0500

    20260212

--- Server/King/KingGameEngine_CreateGameStateDtoForPlayer.cs
+++ Server/King/KingGameEngine_CreateGameStateDtoForPlayer.cs
@@ -11,17 +11,17 @@ public partial class KingGameEngine
         if (player != null)
         {
             state.YourHand = player.Hand
-                .OrderBy(c => c.Rank switch
+                .OrderBy(c => c.Suit switch
+                {
+                    "Spades" => 0, "Clubs" => 1, "Diamonds" => 2, "Hearts" => 3,
+                    _ => 99
+                })
+                .ThenBy(c => c.Rank switch
                 {
                     "2" => 0, "3" => 1, "4" => 2, "5" => 3, "6" => 4,
                     "7" => 5, "8" => 6, "9" => 7, "10" => 8,
                     "J" => 9, "Q" => 10, "K" => 11, "A" => 12,
                     _ => -1
-                })
-                .ThenBy(c => c.Suit switch
-                {
-                    "Spades" => 0, "Clubs" => 1, "Diamonds" => 2, "Hearts" => 3,
-                    _ => 99
                 })
                 .ToList();
             state.TrumpSelectionCards = player.TrumpSelectionCards?.ToList();
